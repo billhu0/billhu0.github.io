acae40d578e80cbe597681fd550258b9ad826910
billhu0 <shhuluojia@163.com>
2022-02-26 18:20:58 +0800

    Site updated: 2022-02-26 18:20:58

--- js/duration_en.js
+++ js/duration_en.js
@@ -22,8 +22,8 @@
 		if (String(snum).length === 1) {
 			snum = "0" + snum;
 		}
-		document.getElementById("timeDate").innerHTML = "本站已运行&nbsp" + dnum + "&nbsp天";
-		document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+		document.getElementById("timeDate").innerHTML = "This site has been running for&nbsp" + dnum + "&nbspdays";
+		document.getElementById("times").innerHTML = hnum + "&nbsphrs&nbsp" + mnum + "&nbspmins&nbsp" + snum + "&nbspseconds";
 	}
 
 	update();
