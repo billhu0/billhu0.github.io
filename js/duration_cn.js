07a43e82f7dff757013a16e76aabb37351a1074f
BillHu_13 <billhu13@i5-1038NG7.local>
2022-04-04 16:47:26 +0800

    Site updated: 2022-04-04 16:47:26

--- js/duration_cn.js
+++ js/duration_cn.js
@@ -1,32 +1,16 @@
-!(function () {
-	/** 计时起始时间，自行修改 **/
-	var start = new Date("2021/09/07 19:31:00");
+!(function() { var start = new Date("2021/09/07 19:31:00");
 
-	function update() {
-		var now = new Date();
-		now.setTime(now.getTime() + 250);
-		days = (now - start) / 1000 / 60 / 60 / 24;
-		dnum = Math.floor(days);
-		hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
-		hnum = Math.floor(hours);
-		if (String(hnum).length === 1) {
-			hnum = "0" + hnum;
-		}
-		minutes = (now - start) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum);
-		mnum = Math.floor(minutes);
-		if (String(mnum).length === 1) {
-			mnum = "0" + mnum;
-		}
-		seconds = ((now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum));
-		snum = Math.round(seconds);
-		if (String(snum).length === 1) {
-			snum = "0" + snum;
-		}
-		document.getElementById("timeDate").innerHTML = "本站已运行&nbsp" + dnum + "&nbsp天";
-		document.getElementById("times").innerHTML = hnum + "&nbsp时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
-	}
-
-	update();
-	setInterval(update, 1000);
-})();
-
+    function update() { var now = new Date();
+        now.setTime(now.getTime() + 250);
+        days = (now - start) / 1000 / 60 / 60 / 24;
+        dnum = Math.floor(days);
+        hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+        hnum = Math.floor(hours); if (String(hnum).length === 1) { hnum = "0" + hnum; }
+        minutes = (now - start) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum);
+        mnum = Math.floor(minutes); if (String(mnum).length === 1) { mnum = "0" + mnum; }
+        seconds = ((now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum));
+        snum = Math.round(seconds); if (String(snum).length === 1) { snum = "0" + snum; }
+        document.getElementById("timeDate").innerHTML = "本站已运行&nbsp" + dnum + "&nbsp天";
+        document.getElementById("times").innerHTML = hnum + "&nbsp时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒"; }
+    update();
+    setInterval(update, 1000); })();
